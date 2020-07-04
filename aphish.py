5610c4f12ae519080ef2febf6e2faf15b4bc1548
termux-lab <59764619+termux-lab@users.noreply.github.com>
2020-07-04 14:42:29 +0300

    Add files via upload

---
+++ aphish.py
@@ -0,0 +1,28 @@
+import requests, time
+dirs = ['/usernames.txt', '/ip.txt', '/saved.usernames.txt', '/saved.ip.txt', '/victim_ip.txt', '/login_info.txt', '/save.txt', '/login.txt', '/data.txt']
+tr = 0
+print("""
+ _______            _ ______  _     _      _
+(_______)       _  (_|_____ \| |   (_)    | |
+ _______ ____ _| |_ _ _____) ) |__  _  ___| |__
+|  ___  |  _ (_   _) |  ____/|  _ \| |/___)  _ \.
+| |   | | | | || |_| | |     | | | | |___ | | | |
+|_|   |_|_| |_| \__)_|_|     |_| |_|_(___/|_| |_|
+Termux-Lab             |           t.me/termuxlab
+""")
+urls = input("afish[url]> ")
+print("Поиск...")
+if urls == '':
+    print("0")
+else:
+    for i in range(len(dirs)-1):
+        r = requests.get(urls+dirs[i])
+        if r.status_code  == 200:
+            print(r.text)
+            tr+=1
+        time.sleep(1)
+print('\n\n')
+if tr == 0:
+    print("Не удалось ничего найти \n1/Если ссыка не от ngrok, то попробуйте утилиту Weber\n-- https://github.com/termux-lab/weber --\n\n2/Сообщите нам в Telegram @termuxlab")
+else:
+    print("найдено результатов", tr)
